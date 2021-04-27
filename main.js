ff6e3ee97928b1b4a5691a5bebe8547f662fab9a
MAD71241 <michele.deliso@gmail.com>
2021-04-27 12:26:37 +0200

    Ok esercizio 3

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //l'utente inserisce due parole in successione
-var firstWord = prompt("Ciao, inserisci una parola!");
+/* var firstWord = prompt("Ciao, inserisci una parola!");
 var secondWord = prompt("Inserisci un'altra parola!")
 
 //stampa entrambe, prima la più lunga e poi la più corta.
@@ -14,15 +14,30 @@ if (firstLength > secondLength) {
 } else {
     console.log(firstWord);
     console.log(secondWord);
-}
+} */
 
 //il software chiede 10 volte all'utente di inserire un numero
 //il programma stampa la somma di tutti i numeri inseriti
-var i;
+/* var i;
 var numbers = [];
 var sum = 0;
 for (i = 0; i < 10; i++) {
     numbers.push(Number(prompt("Inserisci un numero")));
     sum += numbers[i];
 }
-console.log(sum);
+console.log(sum); */
+
+//in un array sono contenuti i nomi dei protagonisti del grande Gatsby
+var names = ["Tizio", "Caio", "Sempronio", "Egidio"];
+
+//chiedi all'utente il suo nome e comunicagli se può partecipare alla festa.
+var myName = prompt("Ciao! Come ti chiami? Inserisci solo il tuo nome :)");
+var i;
+for (i = 0; i<names.length; i++) {
+    if (myName == names[i]) {
+        console.log("Complimenti! Puoi partecipare alla festa.");
+    }
+  else if (i = names.length) {
+    console.log("Spiacente, non sei invitato.");
+}
+}
